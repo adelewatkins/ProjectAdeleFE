00787f27f5e8ac590ae7bcee4821976619208888
adelewatkins <adele_watkins@hotmail.com>
2024-03-18 10:01:34 +0000

    added animation to home screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from './Components/Home';
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import AboutMe from './Components/AboutMe/AboutMe';
+import Dog from './Components/Dog/Dog';
 
 function App() {
   return (
     <div className="App">
+
       <Router>
+        <Link to="/">Home </Link>
+        <Link to="/adele">About Me </Link>
+        <Link to="/dog">Dog</Link>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/adele" element={<AboutMe />} />
+          <Route path="/dog" element={<Dog />} />
         </Routes>
       </Router>
+
     </div>
   );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,34 @@
-function Home() {
-    return (
-        <div>
-            <h1>Home Page</h1>
+import { TypeAnimation } from 'react-type-animation';
 
+function Home() {
+    const ExampleComponent = () => {
+        return (
+            <TypeAnimation
+                // preRenderFirstString={true}
+                sequence={[
+                    'Welcome to the most amazing',
+                    1000,// wait 1s before replacing
+                    'Welcome to the most incredible',
+                    1000,
+                    'Welcome to the best app you have ever seen',
+                    1000,
+                    'Welcome to project Adele',
+                    3000
+
+                ]}
+                wrapper="span"
+                speed={50}
+                deletionSpeed={60}
+                style={{ fontSize: '2em', display: 'inline-block', fontWeight: 'bold', color: 'pink' }}
+                repeat={0}
+            />
+        );
+    };
+    return (
+        <div className="home">
+            <ExampleComponent />
         </div>
-    );
+    )
 }
 
 export default Home
