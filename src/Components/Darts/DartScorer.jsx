a1e592643121deb1ed9184e4cf00873ddc6bd106
adelewatkins <adele_watkins@hotmail.com>
2024-04-10 11:45:20 +0100

    added darts page

--- src/Components/Darts/DartScorer.jsx
+++ src/Components/Darts/DartScorer.jsx
@@ -1,6 +1,23 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 function DartScorer() {
     return ( <div>
         <h1>Darts</h1>
+
+        
+
+        <Form>
+      <Form.Group className="mb-3" controlId="formGroupEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+    </Form>
+
     </div> );
 }
 
