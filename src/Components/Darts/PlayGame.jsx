85f6ef4e5fb8a54b4c86aeb5d32e20eb659a8f67
adelewatkins <adele_watkins@hotmail.com>
2024-04-29 12:45:03 +0100

    updates to play darts page, trying to get running total to store and not reset on refresh

--- src/Components/Darts/PlayGame.jsx
+++ src/Components/Darts/PlayGame.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 
 
+
 function PlayGame() {
 
     const navigate = useNavigate();
@@ -11,25 +12,76 @@ function PlayGame() {
     const [playerOneName, setPlayerOneName] = useState("");
     const [playerTwoName, setPlayerTwoName] = useState("");
     const [gameDetails, setGameDetails] = useState();
+    let [p1Score, setP1Score] = useState()
+    let [p2Score, setP2Score] = useState()
+    let [amountScored, setAmountScored] = useState()
 
-    function getGameDetails(){
+    const handleP1Submit = () => {
+        setP1Score(p1Score - parseInt(amountScored));
+        setAmountScored('');
+        axios.put("http://localhost:8082/darts/update/" + params.id, {
+        p1Score
+      })
+    };
+
+    const handleP2Submit = () => {
+        setP2Score(p2Score - parseInt(amountScored));
+        setAmountScored('');
+
+      axios.put("http://localhost:8082/darts/update/" + params.id, {
+        p2Score
+      })
+    };
+
+    function getGameDetails() {
         axios.get("http://localhost:8082/darts/get/" + params.id)
-        .then((response) => {setGameDetails(response.data)
-            
-        })
-        .catch((err) => console.error(err))
+            .then((response) => {
+                setGameDetails(response.data)
+                setP1Score(response.data.gameType);
+                setP2Score(response.data.gameType);
+            })
+            .catch((err) => console.error(err))
     }
+
     useEffect(getGameDetails, [])
     console.log(gameDetails)
-    
 
 
-    return (  
+
+
+    return (
         <div>
-        <h1>Play Game</h1>
-        
+            <h1>Play Game</h1>
+            <br/>
+            <br/>
+            <div class="col">
+                {gameDetails ? (
+                    <div>
+                        <h2> {gameDetails.playerOneName + " " + p1Score}
+                            {"    "}
+                            {gameDetails.playerTwoName + " " + p2Score}
+                        </h2>
+                    </div>
+                ) : null}
 
+            </div>
+            <div>
+                <form>
+                    <label>
+                        <input
+                        placeholder="Enter amount scored"
+                            type="text"
+                            value={amountScored}
+                            onChange={(e) => setAmountScored(e.target.value)}
+                        />
+                    </label>
+                </form>
+                <button onClick={handleP1Submit}>Submit Player 1 Score</button>
+                <button onClick={handleP2Submit}>Submit Player 2 Score</button>
+                
+            </div>
         </div>
+
     );
 }
 
