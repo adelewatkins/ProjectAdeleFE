b343e4d03702e9167c669b60d16a4400dc5fe0a0
adelewatkins <adele_watkins@hotmail.com>
2024-05-02 15:47:55 +0100

    added pen portrait

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -1,7 +1,13 @@
+import React from 'react';
+import penPortrait from './penportrait.JPG'; // Import the image
+
 function AboutMe() {
     return (
-        <h1>About Me</h1>
-      );
+        <div>
+            <h1>About Me</h1>
+            <img style={{ margin: 'auto', width:'70%' }} src={penPortrait} alt="Pen Portrait" /> {/* Use the imported image */}
+        </div>
+    );
 }
 
 export default AboutMe;
